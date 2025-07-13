0745052319045dab430a450959801f4514946e5c
Fernando Oliveira <fernando.oliveira@solutis.com.br>
2025-07-13 15:23:24 -0300

    Limpando implementações do reader assicrono

--- src/main/java/com/learn/springbatch/SpringBatchApplication.java
+++ src/main/java/com/learn/springbatch/SpringBatchApplication.java
@@ -3,13 +3,10 @@ package com.learn.springbatch;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.batch.JobLauncherApplicationRunner;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBatchApplication implements CommandLineRunner {
--- src/main/java/com/learn/springbatch/step/ProcessaAlunoStep.java
+++ src/main/java/com/learn/springbatch/step/ProcessaAlunoStep.java
@@ -1,18 +1,12 @@
 package com.learn.springbatch.step;
 
-import com.learn.springbatch.client.ExternalApiClient;
 import com.learn.springbatch.dto.AlunosSistemaExternoDTO;
-import com.learn.springbatch.partitioner.AlunoPartitioner;
-import com.learn.springbatch.reader.PagedAlunoItemReader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
